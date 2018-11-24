4a55371c47e638007f29db8ce3aaaf85a0414671
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2018-11-24 19:34:25 +0100

    initial commit

---
+++ src/main/java/com/gearfound/gearfounditemservice/GearfoundItemServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gearfound.gearfounditemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearfoundItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearfoundItemServiceApplication.class, args);
+	}
+}
