4a5ebc1651d611fcec33392ef8371deca50fc821
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2019-01-20 13:19:54 +0100

    formatting cleanup

--- src/test/java/com/gearfound/itemservice/web/LostItemControllerTest.java
+++ src/test/java/com/gearfound/itemservice/web/LostItemControllerTest.java
@@ -13,7 +13,7 @@ import static org.mockito.Mockito.when;
 
 class LostItemControllerTest extends AbstractControllerTest {
 
-       @Test
+    @Test
     void postLostItem() throws Exception {
         //given
         LostItem lostItemInput = new LostItem();
